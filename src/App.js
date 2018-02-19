3e549e9ce530755cb5e49a2c53f84af66ad57a2c
MarloneA <marlone911@gmail.com>
2018-02-19 16:55:59 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          The quick brown fox jumps over the lazy dog.
+        </p>
+        <p>
+          Lorizzle hizzle dolor yo mamma amizzle, boom shackalack adipiscing
+          shizzlin dizzle. Nullam sapizzle velit, aliquet volutpat,
+          suscipizzle shizzlin dizzle, sizzle vizzle, arcu. Pellentesque
+          fo shizzle my nizzle tortizzle. Check out this eros. Dawg izzle get
+          down get down dapibizzle nizzle i'm in the shizzle ma nizzle. Maurizzle
+          pot dang et turpizzle. Nizzle in tortor. Dope eleifend rhoncus shizzlin dizzle.
+           In hizzle break yo neck, yall platea dictumst. That's the shizzle dapibizzle.
+           Fo shizzle my nizzle mah nizzle urna, pretizzle brizzle, mattizzle ac,
+           eleifend vitae, nunc. Fo shizzle my nizzle suscipizzle. Integizzle
+           sempizzle doggy sed purizzle.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
